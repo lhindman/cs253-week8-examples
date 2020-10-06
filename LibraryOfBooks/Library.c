65a1c56d6a6e912502ec1a9b31cdc9cc2239b042
Luke Hindman <lukehindman@boisestate.edu>
2020-10-06 15:13:43 -0700

    Finished library implmentation

--- LibraryOfBooks/Library.c
+++ LibraryOfBooks/Library.c
@@ -7,4 +7,138 @@
  *    in C. 
  */
 
+#include <string.h>
 #include "Library.h"
+
+
+int InitLibrary(Library * libraryPtr, const char name[]) {
+    /* basic parameter validation */
+    if (libraryPtr == NULL || name == NULL) {
+        return -1;
+    }
+
+    /* check the man page for strncpy to see what will happen
+     *    if author is larger than MAX_LIBRARY_FIELD_SIZE-1 */
+    strncpy(libraryPtr->name, name, MAX_LIBRARY_FIELD_SIZE-1);
+
+    /* handle the situation where bookPtr->author is not
+     *   null terminated after strncpy. This has no effect
+     *   on when total copied is less than MAX_BOOK_FIELD_SIZE. */
+    libraryPtr->name[MAX_LIBRARY_FIELD_SIZE-1] = '\0';
+
+    libraryPtr->numBooks = 0;
+
+    return 0;
+}
+
+int AddLibraryBook(Library * libraryPtr, Book * bookPtr) {
+    /* basic parameter validation */
+    if (libraryPtr == NULL || bookPtr == NULL) {
+        return -1;
+    }
+
+    /* Check if the library is full */
+    if (libraryPtr->numBooks >= MAX_LIBRARY_BOOKS) {
+        return -1;
+    }
+
+    /* Add the book */
+    int index = libraryPtr->numBooks;
+    memcpy(&libraryPtr->books[index], bookPtr, sizeof(Book));
+    libraryPtr->isAvail[index] = true;
+    libraryPtr->numBooks++;
+
+    return 0;
+}
+
+int RemoveLibraryBook(Library * libraryPtr, int bookIndex) {
+    /* basic parameter validation */
+    if (libraryPtr == NULL) {
+        return -1;
+    }
+
+    /* Check if the bookIndex is out of range */
+    if (bookIndex < 0 || bookIndex >= libraryPtr->numBooks) {
+        return -1;
+    }
+
+    /* Remove the book by shifting books positioned after the
+     *    specified index down by one */
+    for (int i = bookIndex + 1; i < libraryPtr->numBooks; i++) {
+        memcpy(&libraryPtr->books[i-1], &libraryPtr->books[i], sizeof(Book));
+        libraryPtr->isAvail[i-1] = libraryPtr->isAvail[i-1];
+    }
+    libraryPtr->numBooks--;
+
+    return 0;
+
+}
+
+Book * CheckoutLibraryBook(Library * libraryPtr, int bookIndex) {
+    /* basic parameter validation */
+    if (libraryPtr == NULL) {
+        return NULL;
+    }
+
+    /* Check if the bookIndex is out of range */
+    if (bookIndex < 0 || bookIndex >= libraryPtr->numBooks) {
+        return NULL;
+    }
+
+    /* Check if book is available */
+    if (libraryPtr->isAvail[bookIndex] == false) {
+        return NULL;
+    }
+
+    /* Checkout the book */
+    libraryPtr->isAvail[bookIndex] = false;
+    return &libraryPtr->books[bookIndex];
+}
+
+int ReturnLibraryBook(Library * libraryPtr, Book * bookPtr) {
+    /* basic parameter validation */
+    if (libraryPtr == NULL || bookPtr == NULL) {
+        return -1;
+    }
+
+    /* find the index of the book specified by bookPtr within the library */
+    int index = -1;
+    for (int i = 0; i < libraryPtr->numBooks; i++) {
+        if (&libraryPtr->books[i] == bookPtr) {
+            index = i; 
+            break;
+        }
+    }
+
+    /* Check if book was found */
+    if (index < 0) {
+        return -1;
+    }
+
+    /* Return the book */
+    libraryPtr->isAvail[index] = true;
+
+    return 0;
+}
+
+int DisplayLibraryBooks(Library * libraryPtr) {
+    /* basic parameter validation */
+    if (libraryPtr == NULL) {
+        return -1;
+    }
+    int rc;
+    const int BOOK_BUFFER_SIZE = 100;
+    char bookBuffer[BOOK_BUFFER_SIZE];
+
+    printf("**************************************\n");
+    printf("%s\n",libraryPtr->name);
+    printf("**************************************\n");
+    for(int i = 0; i < libraryPtr->numBooks; i++) {
+        rc = BookToString(&libraryPtr->books[i], bookBuffer, BOOK_BUFFER_SIZE);
+        if (rc < 0) {
+            printf("Error reading book at index: %d\n", i);
+        } else {
+            printf("%s\n",bookBuffer);
+        }
+    }
+}
--- LibraryOfBooks/Library.h
+++ LibraryOfBooks/Library.h
@@ -9,7 +9,9 @@
 #ifndef __LIBRARY_H
 #define __LIBRARY_H
 
+#include <stdbool.h>
 #include "Book.h"
+
 
 const int MAX_LIBRARY_FIELD_SIZE = 60;
 const int MAX_LIBRARY_BOOKS = 8;
@@ -17,20 +19,75 @@ const int MAX_LIBRARY_BOOKS = 8;
 /* define library struct */
 struct library {
     char name[MAX_LIBRARY_FIELD_SIZE];  /* The name of this library :) */
-    Book books[MAX_LIBRARY_BOOKS];     /* Array of book structs */
-    bool isAvail[MAX_LIBRARY_BOOKS];   /* Availability status of cooresponding book by index */
-    int numBooks = 0; /* Number of books currently stored in library */
+    Book books[MAX_LIBRARY_BOOKS];      /* Array of book structs */
+    bool isAvail[MAX_LIBRARY_BOOKS];    /* Availability status of cooresponding book by index */
+    int numBooks;                       /* Number of books currently stored in library */
 };
 
 /* define Library type */
 typedef struct library Library;
 
+/* 
+ * InitLibrary: Initialize the specified library struct
+ *      to set all the fields to good values.
+ * libraryPtr: Pointer to library struct to be initialized
+ * name: Null terminated string 
+ * 
+ * return: 0 on success, -1 on error  
+ */
+int InitLibrary(Library * libraryPtr, const char name[]);
 
-int SetLibraryName(Library * libraryPtr, const char name[]);
+/* 
+ * AddLibraryBook: Make of copy of the Book struct referenced
+ *    by bookPtr and store it in the Library struct referenced by
+ *    libraryPtr.
+ * libraryPtr: Pointer to library struct to be initialized
+ * bookPtr: Pointer to book struct to be copied into library
+ * 
+ * return: 0 on success, -1 on error  
+ */
 int AddLibraryBook(Library * libraryPtr, Book * bookPtr);
+
+/* 
+ * RemoveLibraryBook: Remove the book at the specified index
+ *     from the library.  This will require shifting all of
+ *     the subsequent books down by one to fill the gap.
+ * libraryPtr: Pointer to library struct 
+ * bookIndex: Index of book to be removed.
+ * 
+ * return: 0 on success, -1 on error  
+ */
 int RemoveLibraryBook(Library * libraryPtr, int bookIndex);
+
+/* 
+ * CheckoutLibraryBook:  Determine if the book at the specified
+ *     index is available and if so, mark it as unavailable and
+ *     teturn a pointer to the book struct
+ *     at the specified index. Return NULL if unavailable or error 
+ * libraryPtr: Pointer to library struct 
+ * bookIndex: Index of book to be checked out
+ * 
+ * return: Book pointer or NULL if unavailable/error  
+ */
 Book * CheckoutLibraryBook(Library * libraryPtr, int bookIndex);
+
+/* 
+ * ReturnLibraryBook: Lookup the Book specified by bookPtr
+ *      and set its status to available.
+ * libraryPtr: Pointer to library struct 
+ * bookPtr: Pointer to book struct to be returned
+ * 
+ * return: 0 on success, -1 on error  
+ */
 int ReturnLibraryBook(Library * libraryPtr, Book * bookPtr);
-void DisplayLibraryBooks(Library * libraryPtr);
+
+/* 
+ * DisplayLibraryBooks: Print the list of library books
+ *       along with their availability status to stdout.
+ * libraryPtr: Pointer to library struct 
+ * 
+ * return: 0 on success, -1 on error  
+ */
+int DisplayLibraryBooks(Library * libraryPtr);
 
 #endif /* __LIBRARY_H */
