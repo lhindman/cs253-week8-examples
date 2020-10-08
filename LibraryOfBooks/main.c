96f02e8f08cc3e89599dcdb3de18810649a8b35f
Luke Hindman <lukehindman@boisestate.edu>
2020-10-08 09:52:40 -0700

    Working LibraryOfBooks user interface

--- LibraryOfBooks/main.c
+++ LibraryOfBooks/main.c
@@ -6,6 +6,11 @@
  */
 
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <termios.h>
+#include <unistd.h>
 #include "Library.h"
 #include "Book.h"
 
@@ -13,10 +18,26 @@
  *    it is allocated in the Data Segment */
 Library mylib;
 
+/* Declare "private" helper functions */
+void BufferPurge(void);
+void DisplayMenu(void);
+void UserPressAnyKey(void);
+void UserAddBook(void); 
+void UserRemoveBook(void);
+Book * UserCheckoutBook(void);
+Book * UserReturnBook(Book * myBookPtr);
+
 int main(void) {
 
+	/* Variables */
+	bool running = true;
+	char selection;
+	Book * lightReadingPtr = NULL;
+
+	/* Initialize the Library object*/
 	InitLibrary(&mylib,"Hindman Public Library");
 
+	/* Prepopulate the Library with books */
 	Book a = {"Eye Of The World", "Robert Jordan", 782};
 	AddLibraryBook(&mylib,&a);
 
@@ -29,16 +50,188 @@ int main(void) {
 	Book d = {"Shadow Rising", "Robert Jordan", 1007};
 	AddLibraryBook(&mylib,&d);
 
-	Book * lightReadingPtr = CheckoutLibraryBook(&mylib,2);
+	while (running) {
+		DisplayMenu();
 
-	DisplayLibraryBooks(&mylib);
+		/* Prompt the user to select an option */
+		printf("Menu Selection: ");
+		scanf("%c", &selection);
+		selection = tolower(selection);
+		BufferPurge();
 
-	ReturnLibraryBook(&mylib,lightReadingPtr);
+		switch(selection) {
+			case 'a':
+				UserAddBook();
+				UserPressAnyKey();
+				break;
+			case 'd':
+				UserRemoveBook();
+				UserPressAnyKey();
+				break;
+			case 'c':
+				if (lightReadingPtr != NULL) {
+					printf("Please return %s by %s before checking out another book\n", 
+									lightReadingPtr->title, lightReadingPtr->author);
+				} else {
+					lightReadingPtr = UserCheckoutBook();
+				}
+				UserPressAnyKey();
+				break;
+			case 'r':
+				if (lightReadingPtr == NULL) {
+					printf("No book has been checked out\n");
+				} else {
+					lightReadingPtr = UserReturnBook(lightReadingPtr);
+				}
+				UserPressAnyKey();
+				break;
+			case 'l':
+				DisplayLibraryBooks(&mylib);
+				UserPressAnyKey();
+				break;	
+			case 'q':
+				running = false;
+				break;
+			default:
+				printf("Error: unknown option selected\n");
+				printf("Please choose an option from the menu\n");
+				UserPressAnyKey();
+		}
+	}
+
+	/* Cleanup unused resources */
+
+	exit(0);
+}
+
+void BufferPurge(void) {
+   char c = getchar();
+   while(c != '\n' && c != EOF) {
+      c = getchar();
+   }
+}
+
+void DisplayMenu(void) {
+	printf("-------------------\n");
+	printf("|   Library Menu  |\n");
+	printf("-------------------\n");
+	printf("(A)dd a book (official use only)\n");
+	printf("(D)elete a book (official use only)\n");
+	printf("(C)heckout a book\n");
+	printf("(R)eturn a book\n");
+	printf("(L)ist books\n");
+	printf("(Q)uit\n");
+}
+
+void UserAddBook(void) {
+	/* Create a temporary book objects to store
+	 *   the new book details */
+	Book newBook;
+	printf("Please enter the title:\n");
+	scanf("%59[^\n]", newBook.title);
+	BufferPurge();
+
+	printf("Please enter the author:\n");
+	scanf("%59[^\n]", newBook.author);
+	BufferPurge();
+
+	printf("Please enter the number of pages:\n");
+	while (scanf("%d", &newBook.pages) < 1) {
+		printf("Please enter an integer value");
+		BufferPurge();
+	}
+	BufferPurge();
+
+	/* Add book to library.  This works because the newBook object
+	 *   is copied into the Library. Validate the return code to
+	 *   ensure the book is added successfully. */
+	if (AddLibraryBook(&mylib,&newBook) < 0) {
+		printf("Error occurred while adding book to library\n");
+	} else {
+		printf("Book added successfully!\n");
+	}
+}
+
+void UserRemoveBook(void) {
+	int index;
+
+	/* Prompt for index of book to remove and validate an integer
+	 *    value has been specified */
+	printf("Please enter the index number of the book to be removed:\n");
+	while (scanf("%d", &index) < 1) {
+		printf("Please enter an integer value");
+		BufferPurge();
+	}
+	BufferPurge();
+
+	/* Remove book and validate the return code to ensure the book 
+	 *    is removes successfully */
+	if (RemoveLibraryBook(&mylib,index) < 0) {
+		printf("Error occurred while removing book from library\n");
+	} else {
+		printf("Book removed successfully!\n");
+	}
+}
+
+Book * UserCheckoutBook(void) {
+	int index;
+	Book * myBookPtr = NULL;
+
+	/* Prompt for index of book to checkout and validate an integer
+	 *    value has been specified */
+	printf("Please enter the index number of the book to be checked out:\n");
+	while (scanf("%d", &index) < 1) {
+		printf("Please enter an integer value");
+		BufferPurge();
+	}
+	BufferPurge();
+
+	/* Checkout book and validate the return code to ensure the book 
+	 *    checkout completes successfully */
+	if ( (myBookPtr = CheckoutLibraryBook(&mylib,index)) == NULL) {
+		printf("Error occurred while checking out book from library\n");
+	} else {
+		printf("Successfully checked out %s\n", myBookPtr->title);
+	}
+
+	return myBookPtr;
+}
+
+Book * UserReturnBook(Book * myBookPtr ) {
 	
-	DisplayLibraryBooks(&mylib);
+	/* Checkout book and validate the return code to ensure the book 
+	 *    checkout completes successfully */
+	if (ReturnLibraryBook(&mylib, myBookPtr) < 0) {
+		printf("Error occurred while returning book to library\n");
+		return myBookPtr;
+	} else {
+		printf("Successfully returned %s\n", myBookPtr->title);
+		return NULL;
+	}
+}
 
-	RemoveLibraryBook(&mylib,0);
+/* This gem is something I've kept around for more than 15 years
+ *    as it allows me to replicate the functionality of
+ *    the "Press any key to continue" pause function in DOS.
+ * 
+ *    This is specific to Linux / Unix type systems, and I while
+ *    I am not the original author you are welcome to use it 
+ *    in your own code */
+void UserPressAnyKey(void) {
+	struct termios oldt, newt;
 
-	DisplayLibraryBooks(&mylib);
+	printf("Press any key to continue...\n");
+
+	/* Store the terminal attributes for STDIN */
+	tcgetattr ( STDIN_FILENO, &oldt );
+	newt = oldt;
+	/* Modify the STDIN attributes to disable line buffering */
+	newt.c_lflag &= ~( ICANON | ECHO );
+	tcsetattr ( STDIN_FILENO, TCSANOW, &newt );
+	/* Retrieve a single keystoke from the keyboard without 
+	 *    waiting for enter, but throw away the value. */
+	getchar();
+	/* Restore original terminal attributes for STDIN */
+	tcsetattr ( STDIN_FILENO, TCSANOW, &oldt );
 
 }
